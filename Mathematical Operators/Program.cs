c2e2f369c80f831bfb881feecc159a7eb02b987d
Alex <2291187@cegepmontpetit.ca>
2024-08-08 21:33:57 -0400

    Commit

---
+++ Mathematical Operators/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Nombre 1 :");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("Nombre 2 :");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Nombre 3 :");
+int num3 = int.Parse(Console.ReadLine());
+Console.WriteLine($"La somme de ses chiffres est : {num+num2+num3}");
+Console.WriteLine($"La soustraction du premier et deuxième chiffre : {num-num2}");
+Console.WriteLine($"La multiplication du premier et deuxième chiffre : {num*num2}");
+Console.WriteLine($"La division de 20 par 5 : {20/5}");
